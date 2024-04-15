239bfa5a7c3189047f5f7b354fb88ef0e0a1447d
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-15 10:22:02 +0200

    Add texture loading and management in script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,61 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+/**
+ * Textures
+ */
+// const image = new Image() 
+// const texture = new THREE.Texture(image)
+
+// image.onload = () => {
+//   texture.needsUpdate = true
+// }
+
+// image.src = '/textures/door/color.jpg'
+const loadingManager = new THREE.LoadingManager()
+
+
+loadingManager.onStart = () => {
+    console.log('onStart')
+}
+
+
+loadingManager.onLoad = () => {
+    console.log('onLoad')
+}
+
+
+loadingManager.onProgress = () => {
+    console.log('onProgress')
+}
+
+
+loadingManager.onError = () => {
+    console.log('onError')
+}
+
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+colorTexture.colorSpace = THREE.SRGBColorSpace
+
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = 1
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.minFilter = THREE.NearestFilter
+
+
 /**
  * Base
  */
@@ -14,7 +69,8 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+console.log(geometry.attributes)
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
